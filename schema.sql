7bb3e8e8056ea32f9c01ea38efa9d4b8d13a849b
vvmk <vincentmasiello@gmail.com>
2018-04-20 16:07:11 -0400

    Create schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA promarkone;
+CREATE TABLE promarkone.leads
+(
+    id INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    employer VARCHAR(50),
+    phone VARCHAR(15),
+    tags TEXT
+);
