2a9ba1731c6703d4d19f70b2bc5ad29c1a1212ac
vvmk <vincentmasiello@gmail.com>
2018-04-20 00:57:38 -0400

    Import config

--- export.py
+++ export.py
@@ -1,5 +1,9 @@
-import mysqlclient
-import psycopg2
+#!/usr/bin/env python
+import MySQLdb as mysql
+import psycopg2 as pgsql
+import yaml
 
+with open("config.yaml", 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
 
 
