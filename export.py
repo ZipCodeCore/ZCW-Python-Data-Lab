aa44715bea976d6aca60dcad092ce66e649967b1
vvmk <vincentmasiello@gmail.com>
2018-04-22 22:33:50 -0400

    Pull out tags, use 3 csv/tables, update schema

--- export.py
+++ export.py
@@ -3,9 +3,19 @@ import MySQLdb as mariadb
 import psycopg2 as pgsql
 import yaml
 import csv
+import uuid;
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
+
+tags = {}
+leads_tags = []
+
+def id_for_tag(t):
+    if (t in tags):
+        return tags[t]
+    else:
+        tags[t] = uuid.uuid4()
 
 conn = mariadb.connect(host=cfg['mysql']['host'],
         user=cfg['mysql']['user'],
@@ -14,7 +24,7 @@ conn = mariadb.connect(host=cfg['mysql']['host'],
         port=cfg['mysql']['port'])
 
 leads = []
-
+mariadb_tags_table = {}
 with conn.cursor() as cursor:
     sql = """
         SELECT * FROM proleads;
@@ -23,15 +33,35 @@ with conn.cursor() as cursor:
     result = cursor.fetchall()
     for r in result:
         leads.append({
+            'lead_id': uuid.uuid4(),
             'first_name': r[1],
             'last_name': r[2],
             'email': r[3],
             'gender': r[4],
-            'tags': r[5],
             'phone': None,
-            'employer': None
+            'employer': None,
+            'tags': r[5]
         })
 
+    tag_sql = """
+        SELECT * FROM tags;
+    """
+    cursor.execute(tag_sql)
+    t_result = cursor.fetchall()
+    for t in t_result:
+        mariadb_tags_table[t[0]] = t[1]
+
+for lead in leads:
+    for t in lead['tags'].split(","):
+        try:
+            t_name = mariadb_tags_table[t]
+        except:
+            t_name = "unknown"
+
+        leads_tags.append({
+            'lead_id': lead['lead_id'],
+            'tag_id': id_for_tag(t_name)
+            })
 
 conn_pg = pgsql.connect(host=cfg['pgres']['host'],
         user=cfg['pgres']['user'],
@@ -46,18 +76,40 @@ with conn_pg.cursor() as cursor_pg:
     cursor_pg.execute(sql)
     result = cursor_pg.fetchall()
     for r in result:
-        leads.append({
+        new_lead = {
+            'lead_id': uuid.uuid4(),
             'first_name': r[1],
             'last_name': r[2],
             'email': r[3],
             'gender': None,
-            'tags': r[6],
             'phone': r[4],
-            'employer': r[5]
-        })
+            'employer': r[5],
+            'tags': r[6]
+        }
+        leads.append(new_lead)
+        for t in r[6].split(" "):
+            tag_id = id_for_tag(t)
+            leads_tags.append({
+                'lead_id': new_lead['lead_id'],
+                'tag_id': tag_id
+            })
 
-keys = leads[0].keys()
-with open('leads.csv', 'w') as outfile:
-    dict_writer = csv.DictWriter(outfile, keys)
+lead_keys = leads[0].keys()
+with open('leads.csv', 'w') as lead_file:
+    dict_writer = csv.DictWriter(lead_file,lead_keys)
     dict_writer.writeheader()
     dict_writer.writerows(leads)
+
+with open('tags.csv', 'w') as tag_file:
+    writer = csv.writer(tag_file)
+    writer.writerow(['name', 'tag_id'])
+    for key, value in tags.items():
+       writer.writerow([key, value])
+
+leads_tags.pop(0)
+leads_tags_keys = leads_tags[0].keys()
+with open('lead_tag.csv', 'w') as leads_tag_file:
+    dict_writer = csv.DictWriter(leads_tag_file, leads_tags_keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(leads_tags)
+
--- import.py
+++ import.py
@@ -12,6 +12,18 @@ with open("leads.csv") as infile:
     for row in rows:
         leads.append(row)
 
+tags = []
+with open("tags.csv") as t_infile:
+    t_rows = csv.DictReader(t_infile)
+    for row in t_rows:
+        tags.append(row)
+
+lead_tag = []
+with open("lead_tag.csv") as lt_infile:
+    lt_rows = csv.DictReader(lt_infile)
+    for row in lt_rows:
+        lead_tag.append(row)
+
 conn = mysql.connect(host=cfg['merge']['host'],
         user=cfg['merge']['user'],
         passwd=cfg['merge']['password'],
@@ -19,12 +31,22 @@ conn = mysql.connect(host=cfg['merge']['host'],
         port=cfg['merge']['port'])
 
 conn.cursor().executemany("""
-    INSERT INTO leads (first_name, last_name, email, gender, employer, phone, tags)
-    VALUES (%(first_name)s,
+    INSERT INTO leads (lead_id, first_name, last_name, email, gender, employer, phone)
+    VALUES (%(lead_id)s,
+            %(first_name)s,
             %(last_name)s,
             %(email)s,
             %(gender)s,
             %(employer)s,
-            %(phone)s,
-            %(tags)s)""", leads)
+            %(phone)s)""", leads)
+
+conn.cursor().executemany("""
+        INSERT INTO tags (tag_id, name)
+        VALUES (%(tag_id)s,
+                %(name)s)""",tags)
+
+conn.cursor().executemany("""
+        INSERT INTO lead_tag (lead_id, tag_id)
+        VALUES (%(lead_id)s,
+                %(tag_id)s)""",lead_tag)
 conn.commit()
